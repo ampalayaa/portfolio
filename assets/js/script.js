9711533d5847eea01bcdec2283b7288920f98eaa
ampalayaa <magdayaofrances@gmail.com>
2025-06-12 01:13:33 +0800

    added ionicons script and create a js function for skills toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,29 @@ window.addEventListener("scroll", function () {
         goToBtn.classList.remove("active");
     }
 });
+
+// navbar toggle "NOT WORKING - navbar items not showing"
+
+const navToggleBtn = document.querySelector("[data-nav-toglle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+
+navToggleBtn.addEventListener("ckick", function () {
+    elemToglleFunc(navToggleBtn);
+    elemToglleFunc(navbar);
+    elemToglleFunc(document.body);
+});
+
+
+// skills toggle
+
+const toggleBtnBox = document.querySelector("[data-toggle-box]");
+const toggleBtns = document.querySelector("[data-toggle-btn]");
+const skillsBox = document.querySelector("[data-skills-box]");
+
+for (let i = 0; i < toggleBtns.clientHeight; i++) {
+    toggleBtnBox[i].addEventListener("click", function () {
+        elemToglleFunc(toggleBtnBox);
+        for (let i =0; i <toggleBtns.clientHeight; i++) { elemToglleFunc(toggleBtnBox[i]); }
+        elemToglleFunc(skillsBox);
+    })
+}
