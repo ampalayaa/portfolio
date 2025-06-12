57199687c1cce5a6c98b070859b4c12ac786f04e
ampalayaa <magdayaofrances@gmail.com>
2025-06-12 10:14:33 +0800

    create a js function for the dark and light theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,25 @@ navToggleBtn.addEventListener("ckick", function () {
     elemToglleFunc(document.body);
 });
 
+// dark theme and light theme
+const themeToggleBtn = document.querySelector("[data-theme-btn]");
+
+themeToggleBtn.addEventListener("click", function () {
+    elemToglleFunc(themeToggleBtn);
+
+if (themeToggleBtn.classList.contains("active")) {
+    document.body.classList.remove("dark_theme");
+    document.body.classList.add("light_theme");
+
+    localStorage.setItem("theme", "light_theme");
+} else {
+    document.body.classList.add("dark_theme");
+    document.body.classList.remove("light_theme");
+
+    localStorage.setItem("theme", "dark_theme");
+}
+})
+
 
 // skills toggle
 
