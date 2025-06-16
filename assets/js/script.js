a1e7af6450af541aebc4fdb11b5fef2a97c08c8e
ampalayaa <magdayaofrances@gmail.com>
2025-06-16 13:32:12 +0800

    fixed script and css for each section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,105 @@
-// element toggle function
-const elemToglleFunc = function (elem) { elem.classList.toggle("active"); }
+'use strict';
 
-// header sticky &  go to top
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ * header sticky & go to top
+ */
+
 const header = document.querySelector("[data-header]");
-const goToBtn = document.querySelector("[data-go-top]");
+const goTopBtn = document.querySelector("[data-go-top]");
 
 window.addEventListener("scroll", function () {
 
-    if (window.scroll >= 10) {
-        header.classList.add("active");
-        goToBtn.classList.add("active");
-    } else {
-        header.classList.remmove("active");
-        goToBtn.classList.remove("active");
-    }
+  if (window.scrollY >= 10) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+
 });
 
-// navbar toggle "NOT WORKING - navbar items not showing"
 
-const navToggleBtn = document.querySelector("[data-nav-toglle-btn]");
+
+/**
+ * navbar toggle
+ */
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 const navbar = document.querySelector("[data-navbar]");
 
-navToggleBtn.addEventListener("ckick", function () {
-    elemToglleFunc(navToggleBtn);
-    elemToglleFunc(navbar);
-    elemToglleFunc(document.body);
+navToggleBtn.addEventListener("click", function () {
+
+  elemToggleFunc(navToggleBtn);
+  elemToggleFunc(navbar);
+  elemToggleFunc(document.body);
+
 });
 
-// dark theme and light theme "NOT WORKING"
+
+
+/**
+ * skills toggle
+ */
+
+const toggleBtnBox = document.querySelector("[data-toggle-box]");
+const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+const skillsBox = document.querySelector("[data-skills-box]");
+
+for (let i = 0; i < toggleBtns.length; i++) {
+  toggleBtns[i].addEventListener("click", function () {
+
+    elemToggleFunc(toggleBtnBox);
+    for (let i = 0; i < toggleBtns.length; i++) { elemToggleFunc(toggleBtns[i]); }
+    elemToggleFunc(skillsBox);
+
+  });
+}
+
+
+
+/**
+ * dark & light theme toggle
+ */
+
 const themeToggleBtn = document.querySelector("[data-theme-btn]");
 
 themeToggleBtn.addEventListener("click", function () {
-    elemToglleFunc(themeToggleBtn);
 
-if (themeToggleBtn.classList.contains("active")) {
+  elemToggleFunc(themeToggleBtn);
+
+  if (themeToggleBtn.classList.contains("active")) {
     document.body.classList.remove("dark_theme");
     document.body.classList.add("light_theme");
 
     localStorage.setItem("theme", "light_theme");
-} else {
+  } else {
     document.body.classList.add("dark_theme");
     document.body.classList.remove("light_theme");
 
     localStorage.setItem("theme", "dark_theme");
-}
-})
+  }
 
+});
 
-// check and apply  last time selected theme from localStorage
+/**
+ * check & apply last time selected theme from localStorage
+ */
 
 if (localStorage.getItem("theme") === "light_theme") {
-    themeToggleBtn.classList.add("active");
-    document.body.classList.remove("dark_theme");
-    document.body.classList.add("light_theme");
+  themeToggleBtn.classList.add("active");
+  document.body.classList.remove("dark_theme");
+  document.body.classList.add("light_theme");
 } else {
-    themeToggleBtn.classList.remove("active");
-    document.body.classList.remove("light_theme");
-    document.body.classList.add("dark_theme");
+  themeToggleBtn.classList.remove("active");
+  document.body.classList.remove("light_theme");
+  document.body.classList.add("dark_theme");
 }
-
-// skills toggle
-
-const toggleBtnBox = document.querySelector("[data-toggle-box]");
-const toggleBtns = document.querySelector("[data-toggle-btn]");
-const skillsBox = document.querySelector("[data-skills-box]");
-
-for (let i = 0; i < toggleBtns.clientHeight; i++) {
-    toggleBtnBox[i].addEventListener("click", function () {
-        elemToglleFunc(toggleBtnBox);
-        for (let i =0; i <toggleBtns.clientHeight; i++) { elemToglleFunc(toggleBtnBox[i]); }
-        elemToglleFunc(skillsBox);
-    })
-}
-
-
